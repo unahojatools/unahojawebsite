93992f995666f7bffccb7bf074f743a91d94a26d
unahojatools <unahojatools@gmail.com>
2026-02-25 11:15:17 +0100

    Update tools-validadores.js

--- public/assets/js/tools-validadores.js
+++ public/assets/js/tools-validadores.js
@@ -1,1 +1,10 @@
-
+function cleanAlnum(s){ return (s||"").replace(/[^A-Za-z0-9]/g,"").toUpperCase(); }
+function letraDniDesdeNumero(num8){
+  const letras="TRWAGMYFPDXBNJZSQVHLCKE";
+  return letras[Number(num8)%23];
+}
+function mod97(numStr){
+  let r=0;
+  for(const ch of numStr){ r=(r*10+(ch.charCodeAt(0)-48))%97; }
+  return r;
+}
